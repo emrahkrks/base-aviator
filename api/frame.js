a2057a85ed58fdb15ce7e1e0d6072c8a6c004934
emrahkrks <151388701+emrahkrks@users.noreply.github.com>
2026-05-01 17:58:06 +0300

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -1,6 +1,9 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+
   res.status(200).json({
-    ok: true,
+    version: "v1",
+    frame: true,
     message: "Frame working"
   });
-}
+};
